33b6211032f0ee73719e6d46a3411a255db09ba2
ivinay1 <joshivinay822@gmail.com>
2026-07-01 21:59:54 +0530

    Implement log analysis engine

--- scripts/log_monitor.sh
+++ scripts/log_monitor.sh
@@ -16,7 +16,31 @@ then
         then
             echo "Log File Found"
             echo "Ready To Scan..."
-        
+
+            ERROR_COUNT=$(grep -ic "ERROR" '$LOG_FILE')
+            INFO_COUNT=$(grep -ic "INFO" '$LOG_FILE')
+            WARNING_COUNT=$(grep -ic "WARNING" '$LOG_FILE')
+
+            STATUS="OK"
+            if [ "$ERROR_COUNT" -gt 0 ]
+            then
+                STATUS="ALERT"
+            elif [ "$WARNING_COUNT" -gt 0 ]
+            then
+                STATUS="WARNING"
+            else 
+                STATUS="OK"
+            fi
+    
+            echo "====================Linux Monitor Report======================"
+            echo "Log File        :   $LOG_FILE                                "
+            echo "                                                              "
+            echo "INFO Count      :   $INFO_COUNT                              "
+            echo "WARNING Count   :   $WARNING_COUNT                           "
+            echo "ERROR Count     :   $ERROR_COUNT                             "
+            echo "Status          :   $STATUS                                  "
+            echo "=============================================================="
+
             exit 0
         else
             echo "ERROR: Log File '$LOG_FILE' not found"
